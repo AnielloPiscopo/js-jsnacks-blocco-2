e6df25e2e6b67f1750513f302706123fd21926b6
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-25 16:32:39 +0100

    Improved the work

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -3,7 +3,7 @@ const listOfNumbers = [1,3,24,123,4324,75,98,234,4,3434,5,2372347,23434,23453,84
 
 
 let somma = 0;
-for(let i=0 ; i<listOfNumbers.length ; i++){
+for(let i=0 ; i<10 ; i++){
     somma += listOfNumbers[i];
 }
 
