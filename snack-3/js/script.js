2deae3fab1e4632ce92b497ef5602d7864e52d4b
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-25 16:56:24 +0100

    Made other improvements

--- snack-3/js/script.js
+++ snack-3/js/script.js
@@ -1,15 +1,16 @@
-// Calcola la somma dei primi 10 numeri di un array.
+// Calcola la somma e la media dei primi 10 numeri di un array.
 const listOfNumbers = [1,3,24,123,4324,75,98,234,4,3434,5,2372347,23434,23453,845345,234];
 
 
 let somma = 0;
 let average = 0;
+const numberLimit = 10;
 
-for(let i=0 ; i<10 ; i++){
+for(let i=0 ; i<numberLimit ; i++){
     somma += listOfNumbers[i];
 }
 
-average = somma/10;
+average = somma/numberLimit;
 
 console.log(somma);
 console.log(average);
