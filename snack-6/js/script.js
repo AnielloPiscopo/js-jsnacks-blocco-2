c8fca3b58426459b711c917bf43b76c0c3ba24f4
AnielloPiscopo <aniello.piscopo0707@gmail.com>
2022-11-28 12:52:27 +0100

    Improved the work

--- snack-6/js/script.js
+++ snack-6/js/script.js
@@ -1,11 +1,15 @@
 // Crea un array di numeri interi e fai la somma di tutti gli elementi che sono in posizione dispari
 const numberList = [1,2,3,4,1,34,4];
+let evenNumberSum = 0;
 let oddNumberSum = 0;
 
-for(let i=0 ; i<numberList.length ; i++){
-    if(((i+1) % 2) === 1){
-        oddNumberSum += numberList[i];
-    }
+for(let i=0 ; i<numberList.length ; i+=2){
+    evenNumberSum += numberList[i];
 }
 
+for(let i=1 ; i<numberList.length ; i+=2){
+    oddNumberSum += numberList[i];
+}
+
+console.log(evenNumberSum);
 console.log(oddNumberSum);
